f33f915626fe7f00a842d152098a309460cf5f78
Josep <Josep@cuesta>
2021-04-29 18:20:18 +0200

    .

--- app/src/main/java/com/example/proyecto/MainActivity.java
+++ app/src/main/java/com/example/proyecto/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.proyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 import android.os.Bundle;
 
+import com.example.proyecto.TituloPrincipalJuego1Fragment.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
